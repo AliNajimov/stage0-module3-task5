984bcf043fa9ab62d3bb0a044d2d09551fd66c18
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-22 08:58:51 +0500

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         double a = (double) toBeDivided/5;
-        System.out.print(a);
+        System.out.println(a);
     }
 }
